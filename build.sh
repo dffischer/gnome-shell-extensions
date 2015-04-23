e7f7c05547ddee1794f1639fc5ed6f2b777c7f10
XZS <d.f.fischer@web.de>
2015-04-23 18:40:29 +0200

    make build directory customizable

--- build.sh
+++ build.sh
@@ -7,6 +7,8 @@ then
     'with makepkg-template in a temporary directory, leaving the original' \
     'intact apart from the version numbers and moves the source package back.'
   echo 'Templates are taken from a makepkg-templates directory found besides this script.'
+  echo 'Packages are built in a subdirectory of /tmp/build-packages. This' \
+    'path can be overwritten with the environment variable PACKAGE_BUILD_DIR.'
   exit
 fi
 
@@ -17,14 +19,14 @@ do
     echo "$dir does not contain any PKGBUILD"
   else
     pkgname=$(sed -n "s/pkgname=\([\"']\?\)\(.*\)\1$/\2/p;t" "$dir/PKGBUILD")
-    builddir=/tmp/build-packages/$pkgname
+    builddir="${PACKAGE_BUILD_DIR:=/tmp/build-packages}/$pkgname"
     mkdir -p "$builddir"
-    find $dir -mindepth 1 -maxdepth 1 \! -name PKGBUILD -exec cp -Lrt $builddir '{}' +
-    makepkg-template -p "$dir/PKGBUILD" -o $builddir/PKGBUILD --template-dir ${BASH_SOURCE[@]%%/*}/makepkg-templates
-    ( cd $builddir ; makepkg -f ; mkaurball -f )
-    mv $builddir/*.src.tar.gz $builddir/*.pkg.tar* "$dir"
+    find $dir -mindepth 1 -maxdepth 1 \! -name PKGBUILD -exec cp -Lrt "$builddir" '{}' +
+    makepkg-template -p "$dir/PKGBUILD" -o "$builddir/PKGBUILD" --template-dir ${BASH_SOURCE[@]%%/*}/makepkg-templates
+    ( cd "$builddir" ; makepkg -f ; mkaurball -f )
+    mv "$builddir"/*.src.tar.gz "$builddir"/*.pkg.tar* "$dir"
     oldpkgver=$(grep -Po '(?<=^pkgver=)[^ ]*' "$dir/PKGBUILD")
-    newpkgver=$(grep -Po '(?<=^pkgver=)[^ ]*' $builddir/PKGBUILD)
+    newpkgver=$(grep -Po '(?<=^pkgver=)[^ ]*' "$builddir/PKGBUILD")
     if [ $newpkgver != $oldpkgver ]
     then
       sed --follow-symlinks -i "s:^pkgver=[^ ]*:pkgver=$newpkgver:;s:^pkgrel=[^ ]*:pkgrel=1:" "$dir/PKGBUILD"
